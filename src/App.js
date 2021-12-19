efce66d114eef38c7ac57de6066029b16d330199
arif034 <45455917+arif034@users.noreply.github.com>
2021-12-19 11:37:55 +0530

    Added changes in the code Added changes in the code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./styles.css";
 import { Component } from "react";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: true
+    };
+  }
+
+  onLoginClickHandler = (event) => {};
+
   render() {
     return <div className="App"></div>;
   }
--- src/LoginRegister.js
+++ src/LoginRegister.js
@@ -1,5 +1,28 @@
 import { Component } from "react";
+import { render } from "react-dom";
 
 class LoginRegister extends Component {}
 
+render();
+{
+  return (
+    <div>
+      First Name : <input placeholder="First Name" />
+      <br />
+      <br />
+      Last Name : <input placeholder="Last Name" />
+      <br />
+      <br />
+      Email Id : <input placeholder="Name" />
+      <br />
+      <br />
+      Password : <input placeholder="Name" />
+      <br />
+      <br />
+      Confirm Password : <input placeholder="Name" />
+      <br />
+      <br />
+    </div>
+  );
+}
 export default LoginRegister;
